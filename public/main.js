48dbb99ddf12fa995a183b0869467f29c6be4165
Jessy Chung <jessyc@thentia.com>
2017-03-28 21:12:08 -0400

    init

--- public/main.js
+++ public/main.js
@@ -1,18 +1,53 @@
 $(function () {
 
-    $('#fullpage').fullpage();
+    var fragranceApp = {
 
-    fragranceApp = {};
+        init : function () {
+            $('#fullpage').fullpage();
+            $('#goToNext').on('click', function () {
+                $.fn.fullpage.setAllowScrolling(true);
+            });
+            // $('input[name="smell"]').on('click', function () {
+            //     $.fn.fullpage.setAllowScrolling(true);
+            // });
+            $.fn.fullpage.setAllowScrolling(false);
+        },
 
-    fragranceApp.init = function(){
-        $('#animal-select').on("change", function(){
+        getLocation : function(){
 
-            var animalName = $(this).find(':selected').text();
-            artApp.updateTitle(animalName);
-            artApp.getPieces(animal);
-        });
+            $('form.inputBox').on('click',function(){
+                $('div.toolTip').show();
+            });
+
+            $('form.inputBox').on('submit', function(e){
+                e.preventDefault();
+                var location = $(this).find(".inputText").val();
+                weatherWidget.getWeather(location);
+            });
+
+        },
+
+        getWeather: function(location){
+            var weatherLocation = encodeURIComponent(location);
+            $.ajax('http://api.wunderground.com/api/c674f46e6a835ea2/conditions/q/'+ weatherLocation + '.json', {
+                type : 'GET',
+                dataType : 'jsonp',
+                success : function(currentData){
+                    weatherWidget.parseData(currentData);
+                    $.ajax('http://api.wunderground.com/api/c674f46e6a835ea2/forecast/q/'+ weatherLocation + '.json', {
+                        type : 'GET',
+                        dataType : 'jsonp',
+                        success : function(forecastData){
+                            weatherWidget.parseForecastData(forecastData);
+                            weatherWidget.updateDOM();
+                        }
+                    });
+                }
+            });
+        },
     };
 
+    fragranceApp.init();
 
 
     $('#submit').click(function(){
